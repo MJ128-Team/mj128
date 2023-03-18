32a8bea52ee67178764cb14e35bf64b770021d05
Sergio Lecuona <serlecu@hotmail.com>
2023-03-18 19:58:47 +0100

    Update PowerUp.cs
    - random position
    - destroy when passedBy out of camera

--- Mini-Jam-128/Assets/Scripts/PowerUp.cs
+++ Mini-Jam-128/Assets/Scripts/PowerUp.cs
@@ -4,8 +4,24 @@ using UnityEngine;
 
 public class PowerUp : MonoBehaviour
 {
+    private Camera mainCam;
+    private float camWidth;
+    private float camHeight;
+
     [SerializeField] private string kind; // TODO: Enum
-    [SerializeField] private float value = 10.0f; // in seconds
+    [SerializeField] private float value = 10.0f; // in seconds 
+
+    //Spawn  
+    [SerializeField] private float spawnOffsetY = 0.0f;  
+
+    void Start()
+    {
+        mainCam = Camera.main;
+        camHeight = mainCam.orthographicSize;
+        camWidth = camHeight * mainCam.aspect;
+
+        RandomPosition();
+    }
 
     public void OnCollect()
     {
@@ -16,5 +32,19 @@ public class PowerUp : MonoBehaviour
           // Efecto de particulas
           Destroy(gameObject);
         }
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        if(other.gameObject.tag == "MainCamera")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void RandomPosition()
+    {
+        float spawnPosX = Random.Range(-camWidth * 0.8f, camWidth * 0.8f);
+        float spawnPosY = Random.Range(-camHeight, camHeight);
+        transform.position = new Vector3(spawnPosX, spawnPosY + mainCam.transform.position.y + spawnOffsetY, 0) ;
     }
 }
