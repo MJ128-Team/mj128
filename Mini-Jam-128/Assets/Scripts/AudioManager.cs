4e8d5a12be0fc0c1eaed1cf56b1263423c40b160
Sergio Lecuona <serlecu@hotmail.com>
2023-03-19 12:55:15 +0100

    Update AudioManager.cs
    - add poly-sfx triggering

--- Mini-Jam-128/Assets/Scripts/AudioManager.cs
+++ Mini-Jam-128/Assets/Scripts/AudioManager.cs
@@ -11,6 +11,9 @@ public class AudioManager : MonoBehaviour
     // public AudioSource musicSource;
     [SerializeField] private AudioMixer mixer;
 
+    [SerializeField] private List<AudioSource> sfxSources;
+    [SerializeField] private AudioMixerGroup sfxBus;
+
     public static float musicVolume = 1f;
     public static float sfxVolume = 1f;
 
@@ -27,9 +30,31 @@ public class AudioManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
-    void OnDestroy() {
+    void OnDestroy() 
+    {
         if(instance == this)
             instance = null;
+    }
+
+    void FixedUpdate() 
+    {
+        HandleSfxSources();
+    }
+
+    void HandleSfxSources()
+    {
+        if (sfxSources.Count > 1)
+        {
+          for (int i = 0; i < sfxSources.Count; i++)
+          {
+              if ( i > 0 && !sfxSources[i].isPlaying)
+              { 
+                  // Danger
+                  Destroy(sfxSources[i]);
+                  sfxSources.RemoveAt(i); 
+              }
+          }
+        }
     }
 
     public void SetMusicLevel(float newValue)
@@ -43,5 +68,26 @@ public class AudioManager : MonoBehaviour
         sfxVolume = Mathf.Clamp(newValue, -80.0f, 0f);
         mixer.SetFloat("SfxVol", sfxVolume);
     }
+
+    public void TriggerSfx(AudioClip clip)
+    {
+        bool allBussy = true;
+        foreach (AudioSource source in sfxSources)
+        {
+            if (!source.isPlaying)
+            {
+                source.PlayOneShot(clip);
+                allBussy = false;
+                return;
+            }
+        }
+        if (allBussy)
+        {
+            AudioSource newSource = gameObject.AddComponent<AudioSource>();
+            newSource.outputAudioMixerGroup = sfxBus;
+            newSource.PlayOneShot(clip);
+        }
+    }
+
     
 }
