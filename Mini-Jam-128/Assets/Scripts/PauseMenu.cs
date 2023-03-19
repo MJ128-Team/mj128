cabd2de334ac453310bc1b84e37a5500496ddcbc
Sergio Lecuona <serlecu@hotmail.com>
2023-03-19 10:15:58 +0100

    Update PauseMenu.cs
    - fix setActive
    - all button methods

--- Mini-Jam-128/Assets/Scripts/PauseMenu.cs
+++ Mini-Jam-128/Assets/Scripts/PauseMenu.cs
@@ -28,16 +28,47 @@ public class PauseMenu : MonoBehaviour
 
     public void Resume()
     {
-        transform.GetChild(0).SetActive(false);
+        transform.GetChild(0).gameObject.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
 
     void Pause()
     {
-        transform.GetChild(0).SetActive(true);
+        transform.GetChild(0).gameObject.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
+    }
+
+    public void ToMainMenu()
+    {
+        SceneManager.instance.LoadMainMenu();
+    }
+
+    public void RestartGame()
+    {
+        InGameManager.instance.RestartGame();
+        Resume();
+    }
+
+    public void OpenSettings()
+    {
+        transform.GetChild(1).gameObject.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        transform.GetChild(1).gameObject.SetActive(false);
+    }
+
+    public void OpenMain()
+    {
+        transform.GetChild(0).gameObject.SetActive(true);
+    }
+
+    public void CloseMain()
+    {
+        transform.GetChild(0).gameObject.SetActive(false);
     }
 }
 
