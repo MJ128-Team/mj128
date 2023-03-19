266be3446369cae53d8ba3d8926aaa289580b2a7
Sergio Lecuona <serlecu@hotmail.com>
2023-03-19 14:10:48 +0100

    Update ModalGameOver.cs
    - format timeScores to String
    - get score Text Components
    - modify Text.text on Gameover (display on gameOver Modal)

--- Mini-Jam-128/Assets/Scripts/UI/ModalGameOver.cs
+++ Mini-Jam-128/Assets/Scripts/UI/ModalGameOver.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ModalGameOver : MonoBehaviour
 {
+    [SerializeField] private Text sessionScoreText;
+    [SerializeField] private Text bestScoreText;
+
+    [SerializeField] private string baseScoreText = "SCORE - ";
+    [SerializeField] private string baseBestScoreText = "BEST SCORE - ";
+
+    private string score;
+    private string scoreBest;
+
+
     void Start()
     {
         InGameUIManager.instance.onGameOver += OnGameOver;
@@ -14,8 +25,26 @@ public class ModalGameOver : MonoBehaviour
         InGameUIManager.instance.onGameOver -= OnGameOver;
     }
 
-    void OnGameOver()
+    void OnGameOver(float current, float best)
     {
+        score= ScoreToString(current);
+        scoreBest= ScoreToString(best);
+
         transform.GetChild(0).gameObject.SetActive(true);
+        ApplyScores();
+    }
+
+    string ScoreToString(float timeSecs)
+    {
+        int minutes = Mathf.FloorToInt(timeSecs / 60F);
+        int seconds = Mathf.FloorToInt(timeSecs - minutes * 60);
+        string stringTime = string.Format("{0:0}' {1:00}\"", minutes, seconds);
+        return stringTime;
+    }
+
+    void ApplyScores()
+    {
+        sessionScoreText.text = "SCORE - " + score;
+        bestScoreText.text = "BEST SCORE - " + scoreBest;
     }
 }
