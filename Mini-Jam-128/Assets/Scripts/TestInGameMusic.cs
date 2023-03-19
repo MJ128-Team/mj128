5743934f817d38e3863bb374b2e6329b7f56cc51
Sergio Lecuona <serlecu@hotmail.com>
2023-03-19 19:07:12 +0100

    Create TestInHGameMusic
    script for fast implementing inGame Music

---
+++ Mini-Jam-128/Assets/Scripts/TestInGameMusic.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class TestInGameMusic : MonoBehaviour
+{
+    public static TestInGameMusic instance;
+
+    public AudioSource musicSource;
+    public AudioClip musicClip;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+
+    void OnDestroy()
+    {
+        if (instance == this)
+        {
+            instance = null;
+        }
+    }
+
+    void Start()
+    {
+        musicSource.clip = musicClip;
+    }
+
+    public void PlayLevelMusic()
+    {
+        musicSource.Play();
+    }
+    
+}
